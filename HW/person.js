276a007f43f4918404e154d2cb43a750800ccb95
Dat Nguyen <dnguyenr@uw.edu>
2025-03-04 18:45:39 -0800

    add HW

---
+++ HW/person.js
@@ -0,0 +1,21 @@
+// Questions -
+// Create a Person <few properties and a function to return them> and Inherit it as Student class and override the function
+// Inherit should be done both way's constructor and Object.Create
+// Create three objects and merge their propeties
+// Create a logical example of closure
+// Share few data objects from one file to another
+
+// once done create a git repo and push this answer and send me your github link with access
+
+
+
+var person = {
+    FirstName: "Huy",
+    Address: "Seattle",
+    ID: "0709",
+    getPersonDetails: function() {
+        return `First Name is - ${this.FirstName}
+                Address is - ${this.Address}`
+    }
+}
+
