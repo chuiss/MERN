48dc8972fc813a23e4a31da82d3e697ee894fead
Dat Nguyen <dnguyenr@uw.edu>
2025-03-05 08:34:30 -0800

    update callback, update closure, add functionAsAClass

---
+++ HW/callbackPractice.js
@@ -0,0 +1,5 @@
+//Question :
+//1. create a callback function example for account
+//2. PrintAccount details should accept this call back and the account information
+//3. Upon executing PrintAccntDetails it should show the account details with a message
+//4. Use the same call back to print multiple sessions planned for the day
