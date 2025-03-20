f9acb169b6253e8432c9eda56ce01ad9845e1139
Dat Nguyen <dnguyenr@uw.edu>
2025-03-19 20:19:50 -0700

    forOf and forIn

---
+++ HW/forOf_forIn_loop.js
@@ -0,0 +1,11 @@
+// create your own for loop
+// 1.forOf loop
+
+let artist = {art1: "Doeiichi", art2: "theWeeknd",art3: "Kendrick",art4: "SZA" ,art5: "Frank"};
+
+for(const key in artist){
+    
+}
+
+
+// 2.forIn loop
